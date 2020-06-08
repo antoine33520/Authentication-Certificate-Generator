7cf3e17a9f92955c7493ab74c0641a9492cf6bcd
unknown <eve.andrebrenner@ynov.com>
2020-06-08 16:49:36 +0200

    Versioning 1.0.2 - commentaires debut code certificat

--- certificat.py
+++ certificat.py
@@ -3,3 +3,12 @@
 # Supprimer la phrase secrète de la clé
 # Générer un certificat auto-signé
 
+## Structure d'un certificat :
+#### version de X.509 à laquelle le certificat correspond
+#### numéro de série du certificat
+#### algorithme de chiffrement utilisé pour signer le certificat
+#### nom de l'autorité de certification émettrice
+#### dates de début et de fin de validité
+#### objet de l'utilisation de la clé publique
+#### clé publique du propriétaire du certificat
+#### signature de l'émetteur du certificat
